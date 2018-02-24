15fa5deb29740cde3aa31c4ae1dc7560409874bc
weiliang <1067599390@qq.com>
2018-02-24 21:03:05 +0800

    添加部分配置

---
+++ OperationManager/__init__.py
@@ -0,0 +1,19 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
+# 打开数据库连接
+db = pymysql.connect("47.95.231.82", "manager", "manager123", "operation")
+
+# 使用cursor()操作游标
+cursor = db.cursor()
+
+# 执行sql
+cursor.execute("SELECT VERSION()")
+
+# 使用fetcone()方法获取一条数据
+data = cursor.fetchone()
+
+print("Database version : %s " % data)
+
+# 关闭数据库
+cursor.close()
--- OperationManager/settings.py
+++ OperationManager/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'OperationManager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'operation',
+        'USER': 'manager',
+        'PASSWORD': 'manager123',
+        'HOST': '47.95.231.82',
+        'PORT': '3306'
     }
 }
 
